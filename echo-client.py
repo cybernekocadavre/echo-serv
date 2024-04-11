26b0ce004993baff6526ceeb06888d18014cfe73
cybernekocadavre <124934209+cybernekocadavre@users.noreply.github.com>
2024-04-11 14:06:45 +0300

    Update echo-client.py

--- echo-client.py
+++ echo-client.py
@@ -9,10 +9,12 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = input("Введите имя хоста сервера: ")
 port = int(input("Введите номер порта сервера: "))
 
-client_socket.connect((host, port))
-print("Подключение к серверу установлено.")
+print(f"Попытка подключения к серверу {host}:{port}...")
 
 try:
+    client_socket.connect((host, port))
+    print("Подключение к серверу установлено.")
+
     while True:
         message = input("Введите сообщение для отправки серверу ('exit' для завершения): ")
         if message.lower() == "exit":
