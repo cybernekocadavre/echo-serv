d1f0cbd11c5a658ef4abdb43656f3a40d8f0f2d1
cybernekocadavre <124934209+cybernekocadavre@users.noreply.github.com>
2024-04-11 15:18:10 +0300

    Update echo-serv.py

--- echo-serv.py
+++ echo-serv.py
@@ -3,42 +3,46 @@
 
 # In[ ]:
 import socket
+import logging
+
+# Настройка логгирования
+logging.basicConfig(filename='server.log', level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s: %(message)s')
 
 # Создаем TCP сокет
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Получаем хост и порт для сервера
 host = ''  # Пустая строка означает использование всех доступных интерфейсов
-port = 9091  # Выбираем порт для сервера
+port = 9090  # Выбираем порт для сервера
 
 # Связываем сокет с хостом и портом
 server_socket.bind((host, port))
 
-while True:
-    # Начинаем прослушивать порт, одновременно обслуживая только одно подключение
-    server_socket.listen(1)
+# Начинаем прослушивать порт, одновременно обслуживая только одно подключение
+server_socket.listen(1)
 
-    print("Сервер запущен. Ожидание подключения...")
+logging.info("Сервер запущен. Ожидание подключения...")
 
-    # Принимаем входящее подключение
-    client_socket, client_address = server_socket.accept()
-    print(f"Подключение от {client_address}")
+# Принимаем входящее подключение
+client_socket, client_address = server_socket.accept()
+logging.info(f"Подключение от {client_address}")
 
-    try:
-        while True:
-            # Принимаем данные от клиента
-            data = client_socket.recv(1024)
-            if not data:
-                break
+try:
+    while True:
+        # Принимаем данные от клиента
+        data = client_socket.recv(1024)
+        if not data:
+            break
 
-            # Отправляем обратно клиенту те же данные в верхнем регистре
-            client_socket.sendall(data.upper())
-            print(f"Принято от клиента: {data.decode('utf-8')}")
-
-    finally:
-        # Закрываем соединение с клиентом
-        client_socket.close()
-        print("Соединение с клиентом закрыто.")
+        # Отправляем обратно клиенту те же данные в верхнем регистре
+        client_socket.sendall(data.upper())
+        logging.info(f"Принято от клиента: {data.decode('utf-8')}")
+finally:
+    # Закрываем соединение с клиентом
+    client_socket.close()
 
 # Закрываем серверный сокет
 server_socket.close()
+logging.info("Сервер остановлен")
+
