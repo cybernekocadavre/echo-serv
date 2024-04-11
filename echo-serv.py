7b7eb20496e914937d3faca72f2927426e85b0f9
cybernekocadavre <124934209+cybernekocadavre@users.noreply.github.com>
2024-04-11 14:11:17 +0300

    Update echo-serv.py

--- echo-serv.py
+++ echo-serv.py
@@ -23,14 +23,14 @@ def welcome_client(client_address):
         file.write(f"{ip_address}:{name}\n")
     return name
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = input("Введите имя хоста для сервера (пусто для использования всех доступных интерфейсов): ")
-port = int(input("Введите номер порта для сервера: "))
+host = sys.argv[1] if len(sys.argv) > 1 else ''
+port = int(sys.argv[2]) if len(sys.argv) > 2 else 12345
 
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((host, port))
 server_socket.listen(1)
 
-print(f"Сервер запущен на {host}:{port}. Ожидание подключения...")
+print("Сервер запущен. Ожидание подключения...")
 
 while True:
     client_socket, client_address = server_socket.accept()
