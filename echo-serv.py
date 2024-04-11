cf14d198c98f92711df269544542b383ae6375de
cybernekocadavre <124934209+cybernekocadavre@users.noreply.github.com>
2024-04-11 14:24:46 +0300

    Update echo-serv.py

--- echo-serv.py
+++ echo-serv.py
@@ -4,41 +4,54 @@
 # In[ ]:
 import socket
 
+def get_host_port():
+    default_host = 'localhost'
+    default_port = 9091
+
+    host = input(f"Введите имя хоста (по умолчанию {default_host}): ") or default_host
+    port = input(f"Введите номер порта (по умолчанию {default_port}): ") or default_port
+
+    return host, int(port)
+
 # Создаем TCP сокет
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Получаем хост и порт для сервера
-host = ''  # Пустая строка означает использование всех доступных интерфейсов
-port = 9091  # Выбираем порт для сервера
+try:
+    host, port = get_host_port()
 
-# Связываем сокет с хостом и портом
-server_socket.bind((host, port))
+    # Связываем сокет с хостом и портом
+    server_socket.bind((host, port))
 
-while True:
-    # Начинаем прослушивать порт, одновременно обслуживая только одно подключение
-    server_socket.listen(1)
+    while True:
+        # Начинаем прослушивать порт, одновременно обслуживая только одно подключение
+        server_socket.listen(1)
 
-    print("Сервер запущен. Ожидание подключения...")
+        print("Сервер запущен. Ожидание подключения...")
 
-    # Принимаем входящее подключение
-    client_socket, client_address = server_socket.accept()
-    print(f"Подключение от {client_address}")
+        # Принимаем входящее подключение
+        client_socket, client_address = server_socket.accept()
+        print(f"Подключение от {client_address}")
 
-    try:
-        while True:
-            # Принимаем данные от клиента
-            data = client_socket.recv(1024)
-            if not data:
-                break
+        try:
+            while True:
+                # Принимаем данные от клиента
+                data = client_socket.recv(1024)
+                if not data:
+                    break
 
-            # Отправляем обратно клиенту те же данные в верхнем регистре
-            client_socket.sendall(data.upper())
-            print(f"Принято от клиента: {data.decode('utf-8')}")
+                # Отправляем обратно клиенту те же данные в верхнем регистре
+                client_socket.sendall(data.upper())
+                print(f"Принято от клиента: {data.decode('utf-8')}")
 
-    finally:
-        # Закрываем соединение с клиентом
-        client_socket.close()
-        print("Соединение с клиентом закрыто.")
+        finally:
+            # Закрываем соединение с клиентом
+            client_socket.close()
+            print("Соединение с клиентом закрыто.")
 
-# Закрываем серверный сокет
-server_socket.close()
+except KeyboardInterrupt:
+    print("\nСервер остановлен.")
+
+finally:
+    # Закрываем серверный сокет
+    server_socket.close()
+
